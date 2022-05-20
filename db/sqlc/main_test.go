224b622b477beef70ecf9bd21ee0df25a4e21e91
Kristoffer <kristoffer@kerok.tech>
2022-05-20 14:31:32 +0200

    Added api endpoints and env file
    Added api endpoints for adding, getting and listing users.
    Added .env support via golang viper for db settings.

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -3,6 +3,7 @@ package db
 import (
 	"database/sql"
 	"github.com/jaswdr/faker"
+	"github.com/kerok-kristoffer/formulating/util"
 	"log"
 	"os"
 	"testing"
@@ -10,21 +11,20 @@ import (
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:eloh@localhost:5454/formulating?sslmode=disable"
-)
-
 var testQueries *Queries
 var F = faker.New()
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	// todo: Add models for Recipies and Inventory
+	// todo: Add models for Recipes and Inventory
 	// todo: Add test files for the rest of the models
-	var err error
 
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatalln("cannot load config:", err)
+	}
+
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 
 	if err != nil {
 		log.Fatalln("Cannot connect to db", err)
