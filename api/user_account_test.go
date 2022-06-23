17d64fd7e6e333105adeb0269dd6df37c8d3b1ee
Kristoffer <kristoffer@kerok.tech>
2022-06-23 11:37:51 +0200

    Added mocked test for api createUser route
    Only added the OK test case, will add others later
    Added custom matcher for hashed passwords

--- api/user_account_test.go
+++ api/user_account_test.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"github.com/golang/mock/gomock"
 	"github.com/jaswdr/faker"
 	mockdb "github.com/kerok-kristoffer/formulating/db/mock"
@@ -14,8 +15,100 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
 )
+
+type eqCreateUserParamsMatcher struct {
+	arg      db.CreateUserParams
+	password string
+}
+
+func (e eqCreateUserParamsMatcher) Matches(x interface{}) bool {
+	params, ok := x.(db.CreateUserParams)
+	if !ok {
+		return false
+	}
+
+	err := util.CheckPassword(e.password, params.Hash)
+	if err != nil {
+		return false
+	}
+
+	e.arg.Hash = params.Hash
+	return reflect.DeepEqual(e.arg, params)
+}
+
+func (e eqCreateUserParamsMatcher) String() string {
+	return fmt.Sprintf("matches arg %v and password %v", e.arg, e.password)
+}
+
+func EqCreateUserParams(arg db.CreateUserParams, password string) gomock.Matcher {
+	return eqCreateUserParamsMatcher{arg, password}
+}
+
+func TestCreateUserAPI(t *testing.T) {
+	user, password, err := randomUserWithPassword()
+	require.NoError(t, err)
+
+	testCases := []struct {
+		name          string
+		body          gin.H
+		buildStubs    func(account *mockdb.MockUserAccount)
+		checkResponse func(recorder *httptest.ResponseRecorder)
+	}{
+		{
+			name: "OK",
+			body: gin.H{
+				"username": user.UserName,
+				"password": password,
+				"fullName": user.FullName,
+				"email":    user.Email,
+			},
+			buildStubs: func(account *mockdb.MockUserAccount) {
+				userParams := db.CreateUserParams{
+					FullName: user.FullName,
+					UserName: user.UserName,
+					Email:    user.Email,
+				}
+				account.EXPECT().
+					CreateUser(gomock.Any(), EqCreateUserParams(userParams, password)).
+					Times(1).
+					Return(user, nil)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+				requireBodyMatchUser(t, recorder.Body, user)
+			},
+		},
+		// TODO: add more cases
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			controller := gomock.NewController(t)
+			defer controller.Finish()
+
+			account := mockdb.NewMockUserAccount(controller)
+			tc.buildStubs(account)
+
+			server := NewServer(account)
+			recorder := httptest.NewRecorder()
+
+			data, err := json.Marshal(tc.body)
+			require.NoError(t, err)
+
+			url := "/users"
+			request, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(data))
+			require.NoError(t, err)
+
+			server.router.ServeHTTP(recorder, request)
+			tc.checkResponse(recorder)
+		})
+	}
+}
 
 func TestGetUserAccountAPI(t *testing.T) {
 	user, err := randomUser()
@@ -117,16 +210,29 @@ func requireBodyMatchUser(t *testing.T, body *bytes.Buffer, user db.User) {
 	require.Equal(t, user, gotUser)
 }
 
+func randomUserWithPassword() (db.User, string, error) {
+	f := faker.New()
+	password := f.Internet().Password()
+	return db.User{
+		ID:       0,
+		UserName: f.Person().FirstName(),
+		Email:    f.Internet().Email(),
+		FullName: f.Person().Name(),
+		Hash:     "",
+	}, password, nil
+}
+
 func randomUser() (db.User, error) {
 	f := faker.New()
-	hashedPassword, err := util.HashPassword(f.Internet().Password())
+	password := f.Internet().Password()
+	hashedPassword, err := util.HashPassword(password)
 	if err != nil {
 		return db.User{}, err
 	}
 
 	return db.User{
 		ID:       f.Int64(),
-		UserName: f.Internet().User(),
+		UserName: f.Person().FirstName(),
 		Email:    f.Internet().Email(),
 		FullName: f.Person().Name(),
 		Hash:     hashedPassword,
