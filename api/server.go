b6cc3c1e0b0e2c18ee4c7ca1c2584c3246a6c458
Kristoffer <kristoffer@kerok.tech>
2022-07-20 10:17:57 +0200

    Added Kubernetes deployment

--- api/server.go
+++ api/server.go
@@ -62,8 +62,10 @@ func (server *Server) setupRouter() {
 }
 
 func corsMiddleware() gin.HandlerFunc {
+
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000") // temporary local front-end: todo kerok - change to actual front-end when hosted
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
