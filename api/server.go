1275f26e289abe25e8faa372c2e596c365911bc2
Kristoffer <kristoffer@kerok.tech>
2022-07-21 21:11:40 +0200

    opened up allow-origin to global for testing

--- api/server.go
+++ api/server.go
@@ -65,7 +65,7 @@ func corsMiddleware() gin.HandlerFunc {
 
 	return func(c *gin.Context) {
 
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "213.204.219.115") // temporary local front-end: todo kerok - change to actual front-end when hosted
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
