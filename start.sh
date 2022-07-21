9a29661290ec84172e09573a164afe515be084ae
kerok-kristoffer <103918655+kerok-kristoffer@users.noreply.github.com>
2022-07-21 11:44:17 +0200

    Ft/kube (#14)
    * Added go mod tidy to deploy-build
    
    * update go version to 1.18 in deploy run
    
    * Updating deploy image to run go1.18.3
    
    * Moved testServer functionality out of main_test in api package
    
    * installing dependencies on deploy image
    
    * installing dependencies on deploy image
    
    * Added default cors middleware
    
    * Added Kubernetes deployment
    
    * resolving access issue on kube setup
    
    * manually setting db source temporarily in start
    
    * reverted back to env db source
    
    * updated db source to aws db
    
    * updated db credentials in Makefile
    
    * corrected db credentials in Makefile
    
    * reverted to dev db credentials

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "run db migration"
-/app/migrate -path /app/migration -database "postgresql://root:eloh@localhost:5432/formulating" -verbose up
+/app/migrate -path /app/migration -database "postgresql://root:admin1234%&@formulating.c88yhjcbemef.eu-north-1.rds.amazonaws.com:5432/formulating" -verbose up
 
 echo "start app"
 exec "$@"
