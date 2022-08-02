bb7005ba98debe996486862b6fbea422b874c931
kerok-kristoffer <103918655+kerok-kristoffer@users.noreply.github.com>
2022-08-02 13:51:36 +0200

    Ft/ingredient api (#24)
    * Added api endpoints for ingredients
    
    * Added configurable allowOrigin to cors middleware

--- util/config.go
+++ util/config.go
@@ -11,6 +11,7 @@ type Config struct {
 	ServerAddress       string        `mapstructure:"SERVER_ADDRESS"`
 	TokenSymmetricKey   string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
 	AccessTokenDuration time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
+	AllowOrigin         string        `mapstructure:"ALLOW_ORIGIN"`
 }
 
 func LoadConfig(path string) (config Config, err error) {
