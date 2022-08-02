ea6df8843ee03c400dd8bd7ee40bfc928a201656
Kristoffer <kristoffer@kerok.tech>
2022-08-02 13:46:44 +0200

    Added configurable allowOrigin to cors middleware

--- api/server.go
+++ api/server.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
@@ -68,8 +69,12 @@ func (server *Server) setupRouter() {
 func corsMiddleware() gin.HandlerFunc {
 
 	return func(c *gin.Context) {
+		config, err := util.LoadConfig("../")
+		if err != nil {
+			log.Fatalln("Cannot load config:", err)
+		}
 
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", config.AllowOrigin)
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
