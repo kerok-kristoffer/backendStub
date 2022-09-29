b44263b1530dac4433a4d88187c005db2f56d1f7
Kristoffer <kristoffer@kerok.tech>
2022-09-29 14:46:26 +0200

    activated allow origin for temporary front-end

--- api/server.go
+++ api/server.go
@@ -75,18 +75,18 @@ func (server *Server) setupRouter() {
 func corsMiddleware() gin.HandlerFunc {
 
 	return func(c *gin.Context) {
-		config, err := util.LoadConfig("../")
+		_, err := util.LoadConfig("../")
 		if err != nil {
 			log.Fatalln("Cannot load config:", err)
 		}
 
-		c.Writer.Header().Set("Access-Control-Allow-Origin", config.AllowOrigin)
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Accept, Origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
 
 		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
+			c.JSON(204, nil)
 			return
 		}
 
